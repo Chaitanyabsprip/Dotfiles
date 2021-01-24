e75f40c17f7c5f3953302bd7f0bacb187b4b5326
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-01-24 23:38:18 +0530

    added picom-ibhagwan from aur

--- install.sh
+++ install.sh
@@ -3,19 +3,22 @@
 sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel vim
 sudo pacman -S base-devel cmake unzip
 
-sudo pacman -S lightdm lightdm-gtk-greeter
-sudo systemctl enable lightdm
-sudo vim -c "norm102G0xf=lClightdm-gtk-greeter" \
-  -c "norm107G0xf=lCqtile"
-
-mkdir ~/Programs ~/.config
-mv ./bin ~/bin -r
 # yay
 cd ~/Programs
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 cd ~
+
+# picom ibhagwan
+sudo yay -S picom-ibhagwan-git
+
+sudo pacman -S lightdm lightdm-gtk-greeter
+sudo systemctl enable lightdm
+sudo ln -s ./lightdm.conf /etc/lightdm/lightdm.conf
+
+mkdir ~/Programs ~/.config
+mv ./bin ~/bin -r
 
 # Neovim
 cd ~/Programs
