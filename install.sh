c42b572ddae5a4d4b0cce1f4c828d1d4f165c9c1
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-01-24 23:01:43 +0530

    added neovim install from source

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #! /bin/bash
 
-sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel git
+sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel vim
+sudo pacman -S base-devel cmake unzip
 
-mkdir ~/Programs
+sudo pacman -S lightdm lightdm-gtk-greeter
+sudo systemctl enable lightdm
+sudo vim -c "norm102G0xf=lClightdm-gtk-greeter" \
+  -c "norm107G0xf=lCqtile"
 
+mkdir ~/Programs ~/.config
+mv ./bin ~/bin -r
 # yay
 cd ~/Programs
-sudo pacman -S base-devel
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 cd ~
 
-sudo pacman -S lightdm lightdm-gtk-greeter
-sudo systemctl enable lightdm
-
+# Neovim
 cd ~/Programs
 git clone https://github.com/neovim/neovim
 cd neovim
-make install
+make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
+ln -sf ./config/nvim ~/.config/nvim
+
