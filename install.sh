78f1b9c4a8c15d663231af50998208734e8658f1
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-01-24 22:04:15 +0530

    fixed cd issues with ~/Programs

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
+sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel git
+
 mkdir ~/Programs
 
 # yay
-cd Programs
+cd ~/Programs
 sudo pacman -S base-devel
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 cd ~
 
-sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel
-
 sudo pacman -S lightdm lightdm-gtk-greeter
 sudo systemctl enable lightdm
+
+cd ~/Programs
+git clone https://github.com/neovim/neovim
+cd neovim
+make install
+sudo make install
