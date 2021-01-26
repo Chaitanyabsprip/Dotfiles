95533008a3c3d0445fa758b0e345a8252ae290e7
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2021-01-26 14:01:45 +0530

    reverted to packages in different commands

--- install.sh
+++ install.sh
@@ -10,12 +10,15 @@ mv ./bin ~/
 echo "---------------------------Updating System-------------------------------"
 sudo pacman -Syu
 echo "----------------------------Installing Packages--------------------------"
-sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel vim cmake unzip ripgrep fd cowsay fortune freetype2 fontconfig pkg-config make libxcb gzip lightdm lightdm-gtk-greeter qtile vimb xcb-util-xrm check libxkbcommon-x11 startup-notification pcmanfm networkmanager htop lightdm lightdm-gtk-greeter rustup xkbcommon
+sudo pacman -S xorg xorg-xinit nitrogen firefox xf86-video-intel vim
 echo "------------------------Installing gcc and base-devel--------------------"
 sudo pacman -S --needed gcc base-devel
+echo "---------------Installing cmake ripgrep fd cowsay and fortune------------"
+sudo pacman -S cmake unzip ripgrep fd cowsay fortune-mod
 
 # paru
 echo "---------------------------Installing paru-------------------------------"
+sudo pacman -S cmake freetype2 fontconfig pkg-config make libxcb rustup gzip
 cd ~/Programs
 pwd
 git clone https://aur.archlinux.org/paru.git
@@ -29,10 +32,11 @@ makepkg -sfi
 echo "---------------------------Installing picom-ibhagwan---------------------"
 paru -S picom-ibhagwan-git
 cd ~/dotfiles
-# ln -sf ~/dotfiles/config/picom ~/.config/picom
+ln -sf ~/dotfiles/config/picom ~/.config/picom
 
 # lightdm
 echo "---------------------------Installing lightdm----------------------------"
+sudo pacman -S lightdm lightdm-gtk-greeter
 sudo systemctl enable lightdm
 sudo systemctl enable lightdm.service
 cd ~/dotfiles
@@ -40,8 +44,9 @@ sudo cp ./lightdm.conf /etc/lightdm/lightdm.conf
 
 # qtile
 echo "---------------------------Installing qtile------------------------------"
+sudo pacman -S qtile
 cd ~/dotfiles
-# ln -sf ~/dotfiles/config/qtile ~/.config/qtile
+ln -sf ~/dotfiles/config/qtile ~/.config/qtile
 
 # alacritty
 echo "---------------------------Installing alacritty--------------------------"
@@ -62,10 +67,15 @@ sudo update-desktop-database
 mkdir -p $fish_complete_path[1]
 cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
 cd ~/dotfiles
-# ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
+ln -sf ~/dotfiles/config/alacritty ~/.config/alacritty
+
+# vimb
+echo "---------------------------Installing vimb-------------------------------"
+sudo pacman -S vimb
 
 # rofi
 echo "---------------------------Installing rofi-------------------------------"
+sudo pacman -S xcb-util-xrm check libxkbcommon-x11 startup-notification
 cd ~/Programs
 pwd
 git clone --recursive https://github.com/DaveDavenport/rofi
@@ -77,8 +87,8 @@ mkdir build && cd build
 make
 sudo make install
 cd ~/dotfiles
-# ln -sf ~/dotfiles/config/rofi ~/.config/rofi
-# ln -sf ~/dotfiles/config/rofi.old ~/.config/rofi.old
+ln -sf ~/dotfiles/config/rofi ~/.config/rofi
+ln -sf ~/dotfiles/config/rofi.old ~/.config/rofi.old
 
 # Neovim
 echo "---------------------------Installing neovim-----------------------------"
@@ -90,7 +100,7 @@ pwd
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 cd ~/dotfiles
-# ln -sf ~/dotfiles/config/nvim ~/.config/nvim
+ln -sf ~/dotfiles/config/nvim ~/.config/nvim
 
 #flutter
 echo "--------------------Installing android studio and vscode-----------------"
@@ -101,7 +111,7 @@ paru -S vscodium-bin
 echo "---------------------------Installing startship--------------------------"
 curl -fsSL https://starship.rs/install.sh | bash
 cd ~/dotfiles
-# ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/config ~/.config
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
